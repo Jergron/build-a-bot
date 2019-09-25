bfb6fd241b5a52d0dd2054874e4335d38c19744d
Jeremy Grondahl <jgrondahl@gsandf.com>
2019-09-25 12:13:19 -0500

    Add cart section. Reading state from the store in two components. Can not mutate state directly, but can read state directly from the store.

---
+++ src/cart/ShoppingCart.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h1>Cart</h1>
+    <table>
+      <thead>
+        <tr>
+          <th class="robot-title">
+            Robot
+          </th>
+          <th class="cost">
+            Cost
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(robot, index) in cart" :key="index">
+          <td class="robot-title">
+            {{robot.head.title}}
+          </td>
+          <td class="cost">
+            {{robot.cost}}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'Cart',
+  computed: {
+    /* You can't mutate state directly in a Vuex.Store,
+        But you can access state directly to read from it. */
+    cart() {
+      return this.$store.state.cart;
+    },
+  },
+};
+</script>
+
+<style scoped>
+  td, th {
+    padding: 5px;
+  }
+  .robot-title {
+    text-align: left;
+    padding-right: 200px;
+  }
+  .cost {
+    text-align: right;
+  }
+</style>

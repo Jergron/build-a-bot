a063e522b72524fd58ccdfbfbcf66477a0e860c6
Jeremy Grondahl <jgrondahl@gsandf.com>
2019-09-25 14:33:43 -0500

    Add axios get call to proxy server

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  /* Anything you add to state you first need to explicitly define it's model. */
   state: {
     cart: [],
+    parts: null,
   },
   mutations: {
     addRobotToCart(state, robot) {
       state.cart.push(robot);
+    },
+    updateParts(state, parts) {
+      state.parts = parts;
+    },
+  },
+  actions: {
+    /* Commit function is how to call mutations on the store. */
+    getParts({ commit }) {
+      axios.get('/api/parts')
+        .then(result => commit('updateParts', result.data))
+        .catch(console.error);
     },
   },
   getters: {
